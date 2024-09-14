ed95d5a6305b8686ca7f660328ef6c111ac6c0bd
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-09-15 00:38:20 +0200

    chore(eslint): Ensure proper linting of test files

--- packages/config-eslint/shared.js
+++ packages/config-eslint/shared.js
@@ -2,12 +2,29 @@ import prettier from 'eslint-plugin-prettier/recommended';
 import tseslint from 'typescript-eslint';
 import json from '@eslint/json';
 import turbo from 'eslint-plugin-turbo';
-import vitest from "@vitest/eslint-plugin";
+import vitest from '@vitest/eslint-plugin';
 
 export function ignoreJson(conf) {
   return {
     ...conf,
     ignores: [...(conf.ignores ?? []), '**/*.json'],
+  };
+}
+export function addTsUnusedRule(conf) {
+  return {
+    ...conf,
+    rules: {
+      ...(conf.rules ?? {}),
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   };
 }
 
@@ -32,24 +49,40 @@ export const configs = [
   },
   {
     rules: {
-      'sort-imports': ['error', {
-        ignoreCase: true,
-        ignoreDeclarationSort: false,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: true
-      }]
-    }
+      'sort-imports': [
+        'error',
+        {
+          ignoreCase: true,
+          ignoreDeclarationSort: false,
+          ignoreMemberSort: false,
+          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+          allowSeparatedGroups: true,
+        },
+      ],
+    },
   },
   prettier,
-  ...tseslint.configs.recommended.map(ignoreJson),
+  ...tseslint.configs.recommended.map(ignoreJson).map(addTsUnusedRule),
   {
-    files: ['__tests__/*.spec.ts'],
+    settings: {
+      node: {
+        version: '22.0',
+      },
+    },
+  },
+  {
+    files: ['__mocks__/**/*.cjs', '__tests__/*.spec.ts'],
     plugins: {
-      vitest
+      vitest,
     },
     rules: {
       ...vitest.configs.all.rules,
+    },
+  },
+  {
+    files: ['__mocks__/**/*.cjs'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
     },
   },
 ];
