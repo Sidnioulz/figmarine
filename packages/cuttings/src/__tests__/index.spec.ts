6c6d1d28e9059610ebbdae56d8c7d4e38046c37b
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-09-15 00:43:00 +0200

    test(cuttings): Make index test less useless

--- packages/cuttings/src/__tests__/index.spec.ts
+++ packages/cuttings/src/__tests__/index.spec.ts
@@ -1,3 +1,10 @@
-describe('@figmarine/cuttings', () => {
-  it.only('', () => {});
+import * as index from '../index';
+
+describe('@figmarine/cuttings - index', () => {
+  it('exports something', () => {
+    expect(index).toBeDefined();
+    expect(index.take).toBeDefined();
+    expect(index.digCutting).toBeDefined();
+    expect(index.plantCutting).toBeDefined();
+  });
 });
