9b1d28c960d9c91ff25de42a586b969adc7039e0
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-09-19 22:43:20 +0200

    feat(cache): Port to cacheable and reach 100% coverage
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- packages/cache/src/constants.ts
+++ packages/cache/src/constants.ts
@@ -4,12 +4,10 @@ import path from 'node:path';
 /**
  * Default path where disk caches will be saved and looked up.
  * Stored in the operating system's temporary files.
- * @type {string}
  */
 export const DEFAULT_CACHE_PATH = path.join(os.tmpdir(), '@figmarine/cache');
 
 /**
  *Number of seconds in a calendar year.
- * @type {number}
  */
 export const YEAR_IN_SECONDS = 31556926;
