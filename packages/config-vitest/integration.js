17c8c81cc8a2c323ab260e9254afead2a520be30
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-09-26 14:36:56 +0200

    chore(config-vitest): Split unit and integ and add TS decls

---
+++ packages/config-vitest/integration.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
+import { coverage } from './shared.js';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    threads: false,
+    setupFiles: ['./src/__tests__/setupIntegration.ts'],
+    include: ['**/__tests__/**/*.integration.ts',],
+    coverage,
+  },
+});
---
+++ packages/config-vitest/shared.js
@@ -0,0 +1,16 @@
+export const coverage = {
+  include: [
+    '**/*.{mjs,mjsx,js,jsx,ts,tsx}',
+    '!*.config.{js,ts,cjs}',
+    '!**/coverage/**',
+    '!**/scripts/**',
+    '!**/__fixtures__/**',
+    '!**/__mocks__/**',
+    '!**/__tests__/**',
+    '!**/__generated__/**',
+    '!**/node_modules/**',
+    '!**/dist/**',
+    '!**/src/debug.ts',
+  ],
+  provider: 'istanbul',
+};
