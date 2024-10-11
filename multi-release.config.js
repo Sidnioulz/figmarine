e4f896b5f705f7399ea88eb4b1fcd81b66f71082
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-10-11 23:26:37 +0200

    chore(release): Mark some packages as not ready yet for release

--- multi-release.config.js
+++ multi-release.config.js
@@ -3,6 +3,8 @@ export default {
     // Never included.
     'packages/config-*',
     // Not ready for release yet.
+    'packages/cuttings',
+    'packages/nursery',
     'packages/eslint-plugin-figma',
   ],
   deps: {
