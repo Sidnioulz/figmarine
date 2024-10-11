83386320e139bee80a97b10e0f8902ac86b79408
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-10-11 23:20:51 +0200

    chore(release): Iterate on release config

--- release.config.js
+++ release.config.js
@@ -1,18 +1,8 @@
 export default {
   branches: ['main'],
   plugins: [
-    [
-      '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-      },
-    ],
-    [
-      '@semantic-release/release-notes-generator',
-      {
-        preset: 'conventionalcommits',
-      },
-    ],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
     '@anolilab/semantic-release-pnpm',
     [
