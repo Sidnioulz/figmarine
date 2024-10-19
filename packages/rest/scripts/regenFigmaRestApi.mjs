d05dbb91b26abfc00e7bc7259a4f4405cb90ab78
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2024-10-19 22:34:44 +0200

    fix(rest): Find a roundabout way to sync the upstream OAS
    Signed-off-by: Steve Dodier-Lazaro <sidnioulz@gmail.com>

--- packages/rest/scripts/regenFigmaRestApi.mjs
+++ packages/rest/scripts/regenFigmaRestApi.mjs
@@ -1,10 +1,41 @@
-import { createRequire } from 'node:module';
+import https from 'node:https';
 import path from 'node:path';
 
 import { generateApi } from 'swagger-typescript-api';
+import { parse } from 'yaml';
 
-const require = createRequire(import.meta.url);
-const spec = require('@figma/rest-api-spec/openapi.json');
+import prettierConfig from '../prettier.config.js';
+
+// import { createRequire } from 'node:module';
+// const require = createRequire(import.meta.url);
+// const spec = require('@figma/rest-api-spec/openapi.json');
+
+// FIXME: Temporarily fetching the YAML spec directly from GitHub until
+// https://github.com/figma/rest-api-spec/pull/18 is fixed upstream.
+const OAS_URL = process.env.FIGMA_BRANCH
+  ? `https://raw.githubusercontent.com/figma/rest-api-spec/refs/tags/${process.env.FIGMA_BRANCH}/openapi/openapi.yaml`
+  : 'https://raw.githubusercontent.com/figma/rest-api-spec/refs/heads/main/openapi/openapi.yaml';
+
+function fetchUrl(url) {
+  return new Promise((resolve, reject) => {
+    https
+      .get(url, (res) => {
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk;
+        });
+        res.on('end', () => {
+          resolve(data);
+        });
+      })
+      .on('error', (err) => {
+        reject(err);
+      });
+  });
+}
+
+const yamlContent = await fetchUrl(OAS_URL);
+const spec = parse(yamlContent);
 
 await generateApi({
   name: 'figmaRestApi.ts',
@@ -12,6 +43,7 @@ await generateApi({
   spec,
   httpClientType: 'axios',
   generateRouteTypes: true,
+  prettier: prettierConfig,
   hooks: {
     /** Hack used to make it possible to pass `cache: false` to request params. */
     onCreateRequestParams: (data) => {
